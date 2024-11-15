43fedf1455e9136cfe931117506f0f00508c6c65
VagabondPeak <peter.ptai.nguyen@gmail.com>
2024-11-14 18:07:51 -0600

    Website is runnable to Homeapge
    *Before when server is ran it would show up with a TemplateNotFound
    
    *Now website will show with homepage,
    
    *however need to add links between pages

--- sewebsite/views.py
+++ sewebsite/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
+def home(request):
+    context = {
+        'message': 'Hello, this is a dynamic message!',
+    }
+    return render(request, 'Homepage.html', context)
+
 def homepage(request):
     return HttpResponse()
